d97ff30b0532edb4baa25474bb02b3b561ffa90c
npiegdon <npiegdon@304b4361-a71f-0410-ad35-abd628867220>
2007-05-23 05:18:48 +0000

    Added test plan.

--- trunk/src/version.h
+++ trunk/src/version.h
@@ -8,6 +8,6 @@
 #include <string>
 
 // See readme.txt for a list of what has changed between versions
-static const std::wstring SynthesiaVersionString = L"0.6.0";
+static const std::wstring SynthesiaVersionString = L"pre 0.6.1";
 
 #endif
